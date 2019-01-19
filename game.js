a97b779b97c76cfac601cad4f325b0228fa66e1d
forrestudios <fcheadle@gmail.com>
2019-01-19 15:00:49 -0700

    got some html into the project and did some tutorial work in game.js. Not a lot done in this commit, but I have to go

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
 //game.js
+
+//requirements
+var fs = require('fs'); //file reader
+var http = require('http'); //for hosting webserver locally
+
+//host the webserver in you browser
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var contents = fs.readFileSync('./index.html', 'utf8');
+	
+	res.end(contents);
+
+}).listen(8080);
+ 
